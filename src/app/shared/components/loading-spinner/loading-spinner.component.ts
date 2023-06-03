6149c3590736dd80d6bbcd98bacb8aaa3a46cd7b
mldamico <matias.lioneldamico@gmail.com>
2023-06-03 18:41:22 -0300

    Loading spinner style

---
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  styles: [
+    `
+      .spinner-container {
+        position: fixed;
+        bottom: 15px;
+        right: 15px;
+        background-color: black;
+        color: white;
+        padding: 5px 10px;
+        border-radius: 20px;
+        display: flex;
+        align-items: center;
+        box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.2);
+      }
+
+      span {
+        margin-left: 5px;
+      }
+    `,
+  ],
+})
+export class LoadingSpinnerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
